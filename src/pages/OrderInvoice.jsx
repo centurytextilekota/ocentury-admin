a3e3e9863bbc3600c303d9136c3f1940ab835d98
Abhishek Yadav <monuy8830@gmail.com>
2025-05-05 17:47:33 +0530

    Refactor PageTitle component visibility and adjust layout in OrderInvoice for improved responsiveness

--- src/pages/OrderInvoice.jsx
+++ src/pages/OrderInvoice.jsx
@@ -99,7 +99,7 @@ const OrderInvoice = () => {
       >
         {!loading && (
           <div className="">
-            <div className="flex lg:flex-row md:flex-row flex-row lg:items-center justify-between pb-4 border-b border-gray-50 dark:border-gray-700 dark:text-gray-300">
+            <div className="flex  sm:flex-row flex-col lg:items-center justify-between pb-4 border-b border-gray-50 dark:border-gray-700 dark:text-gray-300">
               {/* <h1 className="font-bold font-serif text-xl uppercase">
                 {t("InvoicePageTittle")}
                 <p className="text-xs mt-1 text-gray-500">
@@ -110,8 +110,8 @@ const OrderInvoice = () => {
                   </span>
                 </p>
               </h1>  */}
-              <div className=" text-left">
-                <h2 className="lg:flex  text-lg font-serif font-semibold mt-4 lg:mt-0 lg:ml-0 md:mt-0">
+              <div className=" text-left mt-4">
+                <h2 className="lg:flex  text-lg font-serif font-semibold  lg:mt-0 lg:ml-0 md:mt-0">
                   {/* {mode === "dark" ? (
                     <img src={logoDark} alt="kachabazar" width="110" />
                   ) : ( */}
@@ -140,7 +140,7 @@ const OrderInvoice = () => {
                 <PageTitle> {t("InvoicePageTittle")} </PageTitle>
                 <div className="flex flex-row flex-wrap justify-between gap-4 pt-4">
                   {/* Invoice Date */}
-                  <div className="flex flex-col">
+                  <div className="flex flex-col items-center">
                     <span className="font-bold font-serif text-sm uppercase text-gray-600 dark:text-gray-500">
                       {t("InvoiceDate")}
                     </span>
@@ -151,7 +151,7 @@ const OrderInvoice = () => {
 
                   {/* Invoice Status */}
                   <div className="flex flex-col">
-                    <span className="font-bold font-serif text-sm uppercase text-gray-600 dark:text-gray-500">
+                    <span className="font-bold items-center font-serif text-sm uppercase text-gray-600 dark:text-gray-500">
                       {t("InvoiceStatus")}
                     </span>
                     <span className="text-xs mt-1 text-gray-500 flex items-center">
@@ -161,7 +161,7 @@ const OrderInvoice = () => {
 
                   {/* Invoice Number */}
                   <div className="flex flex-col">
-                    <span className="font-bold font-serif text-sm uppercase text-gray-600 dark:text-gray-500">
+                    <span className="font-bold items-center font-serif text-sm uppercase text-gray-600 dark:text-gray-500">
                       {t("InvoiceNo")}
                     </span>
                     <span className="text-sm text-gray-500 dark:text-gray-400">
@@ -169,7 +169,9 @@ const OrderInvoice = () => {
                     </span>
                   </div>
                 </div>
-                <div className="mt-2 font-bold font-serif text-sm uppercase">Ship To</div>
+                <div className="mt-2 font-bold font-serif text-sm uppercase">
+                  Ship To :
+                </div>
                 <span className="text-sm text-gray-500 block">
                   {data?.user_info?.name} <br />
                   {data?.user_info?.address}
@@ -227,7 +229,7 @@ const OrderInvoice = () => {
 
         {!loading && (
           <div className="border rounded-xl border-gray-100 p-8 py-6 bg-gray-50 dark:bg-gray-900 dark:border-gray-800">
-            <div className="flex lg:flex-row md:flex-row flex-col justify-between">
+            <div className="flex sm:flex-row  flex-col justify-between">
               <div className="mb-3 md:mb-0 lg:mb-0  flex flex-col sm:flex-wrap">
                 <span className="mb-1 font-bold font-serif text-sm uppercase text-gray-600 dark:text-gray-500 block">
                   {t("InvoicepaymentMethod")}
